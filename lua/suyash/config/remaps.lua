1ecf41680fbeb98ae28f083365d75af854796f3f
Suyash <suyashsrivastava5053@gmail.com>
2025-07-28 12:57:03 +0000

    removing errors from the snacks and lsp

--- lua/suyash/config/remaps.lua
+++ lua/suyash/config/remaps.lua
@@ -5,4 +5,3 @@ map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
-
